2f6d8daa1c83981db8dd24f030e96fe132773273
LorakPL <karolfur1995@gmail.com>
2018-06-25 22:20:11 +0200

    added sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    AlertModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
